3e75d1127fa3ecf1a482b0c87e87ade59b22fbf7
adityasingh7402 <29117136+adityasingh7402@users.noreply.github.com>
2026-03-01 11:18:47 +0530

    refactor: rename proxy.ts to middleware.ts

---
+++ middleware.ts
@@ -0,0 +1,37 @@
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
+
+const isProtectedRoute = createRouteMatcher(['/dashboard(.*)', '/profile(.*)'])
+const isPublicRoute = createRouteMatcher([
+    '/',
+    '/sign-in(.*)',
+    '/sign-up(.*)',
+    '/api/webhooks(.*)',
+    '/api/receiving-requests(.*)',
+    '/chat(.*)',
+    '/api/chat(.*)'
+])
+
+import { NextResponse } from 'next/server';
+
+export default clerkMiddleware(async (auth, req) => {
+    if (isPublicRoute(req)) {
+        const response = NextResponse.next();
+        response.headers.set('x-is-public-route', 'true');
+        return response;
+    }
+
+    const { isAuthenticated, redirectToSignIn } = await auth()
+
+    if (!isAuthenticated && isProtectedRoute(req)) {
+        return redirectToSignIn()
+    }
+},)
+
+export const config = {
+    matcher: [
+        // Skip Next.js internals and all static files, unless found in search params
+        '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+        // Always run for API routes
+        '/(api|trpc)(.*)',
+    ],
+};
